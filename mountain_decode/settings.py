4a8edfb9db8353dd40271d14a9b7bfb2f8358b3b
w-ikeda <tatikogi1014@gmail.com>
2020-06-01 19:49:03 +0900

    2020.06.01.19:49

--- mountain_decode/settings.py
+++ mountain_decode/settings.py
@@ -131,6 +131,10 @@ STATIC_URL = 'static/'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# メディアファイルをS3に保存
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# collectstatic を実行すると静的ファイルをS3に保存
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
